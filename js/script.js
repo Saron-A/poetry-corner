ea739ed328613b295bcded5a2950078fd673398f
Saron <saronabebe2019@gmail.com>
2025-03-11 18:22:11 -0800

    Add local Storage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
+let dialog = document.querySelector("dialog");
+let form = document.querySelector("#newPoem");
+let showDialog = document.querySelector("#showDialog");
 
+let list = document.querySelector(".poetry-list");
+
+let corner = JSON.parse(localStorage.getItem("corner")) || [];
+
+displayCorner(); // display when the page loads
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let title = document.querySelector("#title");
+  let genre = document.querySelector("#data-category");
+  let date = document.querySelector("#date");
+
+  let newPoem = {
+    title: title.value,
+    genre: genre.value,
+    date: date.value,
+  };
+
+  corner.push(newPoem);
+  localStorage.setItem("corner", JSON.stringify(corner));
+
+  dialog.close();
+  form.reset();
+});
+
+showDialog.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+function displayCorner() {
+  list.innerHTML = "";
+
+  corner.forEach((poem) => {
+    let poemCard = document.createElement("div");
+    poemCard.classList.add("poem-card");
+
+    let title = document.createElement("h3");
+    title.textContent = poem.title;
+
+    let genre = document.createElement("p");
+    genre.textContent = `Genre: ${poem.genre}`;
+
+    poemCard.append(title, genre);
+    list.appendChild(poemCard);
+  });
+}
