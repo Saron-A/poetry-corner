d8822c2bd30c87bfbc04f4a9d2624b7e635c502e
Saron <saronabebe2019@gmail.com>
2025-04-24 14:56:17 -0800

    Make the contents in the database appear on the archive page

--- js/archive.js
+++ js/archive.js
@@ -1,4 +1,16 @@
 let input = document.querySelector("#search");
+
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    let response = await axios.get("http://Localhost:3000/poems");
+    let poems = response.data;
+    console.log(poems);
+    showPoems(poems);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 input.addEventListener("keydown", async (e) => {
   if (e.key === "Enter") {
     const reqTitle = input.value.trim();
@@ -14,3 +26,25 @@ input.addEventListener("keydown", async (e) => {
     input.focus(); // Keep the focus on the input field
   }
 });
+
+function showPoems(poems) {
+  let poetryList = document.querySelector(".poetry-list");
+  poetryList.innerHTML = ""; // Clear the list before displaying new poems
+  poetryList.classList.add("poem-list");
+  poems.forEach((poem) => {
+    let poemCard = document.createElement("div");
+
+    let h3 = document.createElement("h3");
+    h3.textContent = poem.title;
+
+    let h4 = document.createElement("h4");
+    h4.textContent = poem.author;
+
+    let p = document.createElement("p");
+    p.textContent = poem.genre;
+
+    poemCard.append(h3, p, h4);
+    poetryList.appendChild(poemCard);
+    poemCard.classList.add("poem-card");
+  });
+}
