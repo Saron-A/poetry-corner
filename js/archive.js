a858ae1f70384a76e68af3a91ccdac6d1eeae797
Saron <saronabebe2019@gmail.com>
2025-04-24 15:55:06 -0800

    Add search functionality

--- js/archive.js
+++ js/archive.js
@@ -16,8 +16,9 @@ input.addEventListener("keydown", async (e) => {
     const reqTitle = input.value.trim();
     try {
       let response = await axios.get(`http://localhost:3000/poems/${reqTitle}`);
-      let poem = response.data;
-      console.log(poem);
+      let poems = response.data;
+      console.log(poems);
+      displaySearchResults(poems);
     } catch (error) {
       console.error("Error fetching poem:", error);
     }
@@ -48,3 +49,33 @@ function showPoems(poems) {
     poemCard.classList.add("poem-card");
   });
 }
+
+function displaySearchResults(poems) {
+  let searchResults = document.querySelector(".search-results");
+  searchResults.classList.remove("hidden"); // Show results
+  searchResults.innerHTML = ""; // Clear previous results
+
+  const div = document.createElement("div");
+  const h3 = document.createElement("h3");
+  h3.textContent = poems.title;
+
+  const h4 = document.createElement("h4");
+  h4.textContent = `by ${poems.author}`;
+
+  const p = document.createElement("p");
+  p.textContent = `Genre: ${poems.genre}`;
+
+  div.append(h3, p, h4);
+  div.classList.add("search-result");
+
+  searchResults.appendChild(div);
+}
+
+document.addEventListener("click", (e) => {
+  const input = document.querySelector("#search");
+  const results = document.querySelector(".search-results");
+
+  if (!input.contains(e.target) && !results.contains(e.target)) {
+    results.classList.add("hidden");
+  }
+});
