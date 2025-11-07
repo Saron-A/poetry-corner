90798d8b0b07b4db18bdc8ba9c560d6e4d6044d0
Saron <saronabebe2019@gmail.com>
2025-11-07 11:55:43 -0800

    Set up a new server

---
+++ backend/app.js
@@ -0,0 +1,58 @@
+const bcrypt = require("bcryptjs");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+
+app.use(cors());
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+
+// authentication
+//Step 1: initialize passport
+app.use(
+  session({
+    secret: "some secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStorage,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 30, // a month
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Step 2: define a strategy
+// when logging in
+passport.use(
+  new localStrategy(async (username, password, done) => {
+    // fetch user information from the database using the username and check for password
+  })
+);
+
+//Step 3: serialize and deserialize user
+//3.1 serialize
+passport.serializeUser((user, done) => {
+  return done(null, user.id);
+});
+//3.2 deserialize
+passport.deserializeUser(async (id, done) => {
+  // fetch the user from the database using the id
+  //return the user object
+});
+//ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// routes -- mandatory
+
+//check if the server is listening
+app.listen(3000, (err) => {
+  if (err) throw err;
+  console.log("Server running at port 3000");
+});
