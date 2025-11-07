b0319dc1ba8016c2da0a38d82850189710868708
Saron <saronabebe2019@gmail.com>
2025-11-07 13:06:39 -0800

    Set up database connection

---
+++ backend/db/pool.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false, // Needed for Render / Railway / Supabase hosted databases
+  },
+});
+
+module.exports = pool;
