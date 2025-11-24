3293d718f6fde2f0cebdc1507ba9ddc5f0026a2d
Saron <saronabebe2019@gmail.com>
2025-11-23 18:52:46 -0800

    Connect the project to a remote database

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  // ssl: {
-  //   require: true,
-  //   rejectUnauthorized: false, // Needed for Render / Railway / Supabase hosted databases
-  // },
+  connectionString: process.env.DB_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false, // Needed for Render / Railway / Supabase hosted databases
+  },
 });
 
 module.exports = pool;
